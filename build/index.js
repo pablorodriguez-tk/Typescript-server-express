1777b67f88ac10ac4b4a8ca878df24873fed1338
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-17 18:40:45 -0300

    add routes

--- build/index.js
+++ build/index.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes/loginRoutes");
+var cookie_session_1 = __importDefault(require("cookie-session"));
 // Crear el servidor de express
 var app = express_1.default();
-app.get('/', function (req, res) {
-    res.send("\n<div>\n<h1>Hi there<h1/>\n</div>\n");
-});
+// Lectura y parseo del body
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(cookie_session_1.default({ keys: ['asddas'] }));
+// Rutas
+app.use(loginRoutes_1.router);
+// Escuchar peticiones
 app.listen(3000, function () {
     console.log("Servidor corriendo en puerto 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
 
 // Crear el servidor de express
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-<div>
-<h1>Hi there<h1/>
-</div>
-`);
-});
+// Lectura y parseo del body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asddas'] }));
 
+// Rutas
+app.use(router);
+
+// Escuchar peticiones
 app.listen(3000, () => {
   console.log(`Servidor corriendo en puerto 3000`);
 });
